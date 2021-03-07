8c797dde218102d4a8f081c9f6e670301276e58e
Jason Leibowitz <jason@leibowitz.me>
2021-03-07 13:44:40 -0500

    Cleanup DI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TerminusModule } from '@nestjs/terminus';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { UsersController } from './users/users.controller';
 import { configService } from './config/config.service';
-import { HealthController } from './health/health.controller';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
-    TerminusModule,
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
     AuthModule,
     UsersModule,
+    HealthModule,
   ],
-  controllers: [UsersController, HealthController],
   providers: [],
 })
 export class AppModule {}
