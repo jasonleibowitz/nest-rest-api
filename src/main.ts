814c158cd703ba31694d3974667eb3b398ee8548
Jason Leibowitz <jason@leibowitz.me>
2021-03-07 13:24:52 -0500

    Prefix all routes with /api

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
