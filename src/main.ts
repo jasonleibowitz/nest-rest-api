1d03fdfb03a500614053c60c35533c1f335d4f6e
Jason Leibowitz <jason@leibowitz.me>
2021-03-06 21:41:58 -0500

    Base Swagger Setup

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Rest API Template Docs')
+    .setDescription('The API Docs for the REST API Template')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
   await app.listen(3000);
 }
 bootstrap();
